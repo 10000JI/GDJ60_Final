a086a74f386ce98efcf1a3b6b9269b3d08977cab
10000JI <sksk436@naver.com>
2023-05-11 12:25:55 +0900

    setting

---
+++ src/main/java/com/example/demo/Gdj60FinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdj60FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gdj60FinalApplication.class, args);
+	}
+
+}
